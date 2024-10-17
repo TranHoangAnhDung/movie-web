070d72c9e421d5c17eaf76b057652036860fca56
TranHoangAnhDung <thad091099@gmail.com>
2024-10-17 20:18:43 +0700

    loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './loading.css'
+const Loading = () => {
+  return (
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
